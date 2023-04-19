95e2c220c6c1a650edcbddf82ace33727d37f5ae
Alessandro Affinito <affinito.ale@gmail.com>
2023-04-19 11:02:15 +0200

    Add config file

--- build/build-app.sh
+++ build/build-app.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 cd .    # to avoid Make "getcwd: No such file or directory" error
+. ./config/config
+
+sed "s/APP_VERSION/${APP_VERSION}/" ./deploy/evil_service_template.yaml > ./deploy/evil_service.yaml
+sed -i "s/^VERSION ?=.*/VERSION ?= $APP_VERSION/" Makefile
+
 make
 make test
 make container
